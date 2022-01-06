18a1443d16df312d215c4ccf0c79f4ad8369bec7
vladwulf <agaev.vladimir@gmail.com>
2022-01-06 14:51:08 +0000

    clean migrations

--- prisma/migrations/20211204171216_email_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- prisma/migrations/20220106145018_init/migration.sql
+++ prisma/migrations/20220106145018_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
@@ -18,3 +9,6 @@ CREATE TABLE "users" (
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
